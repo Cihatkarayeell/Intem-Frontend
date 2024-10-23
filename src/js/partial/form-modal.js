436ba030bc3a9e766b463528fbec318858d9db8b
Cihat <cihatkarayel@icloud.com>
2024-10-23 15:53:00 +0300

    bug fixed

--- src/js/partial/form-modal.js
+++ src/js/partial/form-modal.js
@@ -1,25 +1,29 @@
-// Modal açma
-document.querySelector('.submit-button').addEventListener('click', function(event) {
-    event.preventDefault(); // Linkin varsayılan davranışını engeller
-    event.stopPropagation(); // Tıklama olayının yukarıya yayılmasını durdurur
-    document.querySelector('.modal-dialog').classList.add('active');
-});
+// Sayfada .form sınıfına sahip bir element olup olmadığını kontrol et
+if (document.querySelector('.form-element')) {
+    // Modal açma
+    document.querySelector('.submit-button').addEventListener('click', function(event) {
+        event.preventDefault(); // Linkin varsayılan davranışını engeller
+        event.stopPropagation(); // Tıklama olayının yukarıya yayılmasını durdurur
+        document.querySelector('.modal-dialog').classList.add('active');
+    });
 
-// Modal kapatma - close butonuna tıklama
-document.querySelector('.modal-dialog .close-button').addEventListener('click', function() {
-    document.querySelector('.modal-dialog').classList.remove('active');
-});
+    // Modal kapatma - close butonuna tıklama
+    document.querySelector('.modal-dialog .close-button').addEventListener('click', function() {
+        document.querySelector('.modal-dialog').classList.remove('active');
+    });
 
-// Modal kapatma - modal-box dışında bir yere tıklama
-document.addEventListener('click', function(event) {
-    const modal = document.querySelector('.modal-dialog');
-    const modalBox = document.querySelector('.modal-box');
-    
-    if (modal.classList.contains('active') && !modalBox.contains(event.target)) {
-        modal.classList.remove('active');
-    }
-});
-// Modal içindeki tıklamaların dışarıya yayılmasını önle
-document.querySelector('.modal-box').addEventListener('click', function(event) {
-    event.stopPropagation(); // modal-box içindeki tıklamaların dışarıya yayılmasını durdurur
-});
+    // Modal kapatma - modal-box dışında bir yere tıklama
+    document.addEventListener('click', function(event) {
+        const modal = document.querySelector('.modal-dialog');
+        const modalBox = document.querySelector('.modal-box');
+
+        if (modal.classList.contains('active') && !modalBox.contains(event.target)) {
+            modal.classList.remove('active');
+        }
+    });
+
+    // Modal içindeki tıklamaların dışarıya yayılmasını önle
+    document.querySelector('.modal-box').addEventListener('click', function(event) {
+        event.stopPropagation(); // modal-box içindeki tıklamaların dışarıya yayılmasını durdurur
+    });
+}
